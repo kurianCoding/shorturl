dfb2ed5b8af9dfe13f320dc6b0d85cd803c70ac0
kurian <kurianck.mail@gmail.com>
2018-04-07 09:20:24 +0530

    Next: check to see if url is valid

--- server.go
+++ server.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
-import "github.com/kurianCoding/urlShort/shortFunc"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kurianCoding/shorturl/shortFunc"
+)
+
+type shortUrlReq struct {
+	LongUrl string `json:"longUrl"`
+}
+type shortUrlResp struct {
+	ShortUrl string `json:"shortUrl"`
+}
 
 func main() {
-	fmt.Println(shortFunc.ShortUrl("https://www.geeksforgeeks.org/find-if-there-is-a-path-between-two-vertices-in-a-given-graph/", 10))
+	r := mux.NewRouter()
+	r.HandleFunc("/shortUrl", shortUrl).Methods("POST")
+	srv := &http.Server{
+		Addr:    "localhost:8080",
+		Handler: r,
+	}
+	srv.ListenAndServe()
 	return
+}
+func shortUrl(w http.ResponseWriter, r *http.Request) {
+	jsonDecoder := json.NewDecoder(r.Body)
+	var t shortUrlReq
+	defer r.Body.Close()
+	err := jsonDecoder.Decode(&t)
+	if err != nil {
+		panic(err)
+	}
+	jsonEncoder := json.NewEncoder(w)
+	jsonEncoder.Encode(shortUrlResp{ShortUrl: shortFunc.ShortUrl(t.LongUrl, 10)})
 }
